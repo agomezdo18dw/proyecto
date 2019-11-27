68569d2037d3ba1f311c6b73bbe5e28c1a7733dd
aimarperuchena <aimarperuchena@gmail.com>
2019-11-27 14:01:48 +0100

    Añadidos 2 puntos mas en el mapa automaticamente con un bucle, para que mas adelante sirva de prueba

--- js/mapa.js
+++ js/mapa.js
@@ -1,42 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var map = L.map('map').
-        setView([43.3227696, -1.9940565, 17], 15);
-        var layer = L.esri.basemapLayer('Physical').addTo(map);
-        var layerLabels;
-        
-        function setBasemap (basemap) {
-            if (layer) {
-              map.removeLayer(layer);
-            }
-        
-            layer = L.esri.basemapLayer(basemap);
-        
-            map.addLayer(layer);
-        
-            if (layerLabels) {
-              map.removeLayer(layerLabels);
-            }
-        
-            if (basemap.includes('Imagery')) {
-              layerLabels = L.esri.basemapLayer('ImageryLabels');
-              map.addLayer(layerLabels);
-            }
-          }
-          document
-          .querySelector('#basemaps')
-          .addEventListener('change', function (e) {
+    setView([43.3227696, -1.9940565, 17], 15);
+    var layer = L.esri.basemapLayer('Physical').addTo(map);
+    var layerLabels;
+
+    function setBasemap(basemap) {
+        if (layer) {
+            map.removeLayer(layer);
+        }
+
+        layer = L.esri.basemapLayer(basemap);
+
+        map.addLayer(layer);
+
+        if (layerLabels) {
+            map.removeLayer(layerLabels);
+        }
+
+        if (basemap.includes('Imagery')) {
+            layerLabels = L.esri.basemapLayer('ImageryLabels');
+            map.addLayer(layerLabels);
+        }
+    }
+    document
+        .querySelector('#basemaps')
+        .addEventListener('change', function (e) {
             var basemap = e.target.value;
             setBasemap(basemap);
-          });
-
+        });
+    let latitud = -1.9921275;
+    let longitud = 43.3228796;
+    for (i = 0; i < 3; i++) {
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-            }).addTo(map);
-            L.control.scale().addTo(map);
-            L.marker([43.3228796, -1.9921275], {
-                draggable: true
-            }).addTo(map);
-            $(L).onclick(function(){
-                    L.popup().setLatLng([43.3228796, -1.9921275]).setContent("Aquarium").openOn(map);
-                });
+        }).addTo(map);
+        L.control.scale().addTo(map);
+        L.marker([longitud, latitud], {
+            draggable: true
+        }).addTo(map);
+        latitud += 1;
+        longitud += 1;
+    }
 });
